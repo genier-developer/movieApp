ff33c5ff65fb35f855742c74d7a955ebd8111601
Olga Genier <genier.developer@gmail.com>
2024-05-24 16:23:12 +0300

    add useApi.tsx

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,5 +1,5 @@
-export enum SerachType {
-    "all"= '',
+export enum SearchType {
+    "all" = '',
     movie = 'movie',
     series = 'series',
     episode = 'episode'
@@ -16,3 +16,26 @@ export interface DetailResult {
     Actors: string
     Website: string
 }
+
+export const useApi = (() => {
+    let url = 'https://www.omdbapi.com/'
+    let apiKey = '33ab72be'
+
+    const searchData = async (title: string, type: SearchType): Promise <SearchType> => {
+        const result = await fetch(
+            `${url}?s=${encodeURI(title)}&type=${type}&apikey=${apiKey}`
+        )
+        return result.json()
+    }
+
+    const getDetails = async (id: string) => {
+        const result = await fetch(
+            `${url}?i=${id}&plot=full&apikey=${apiKey}`
+        )
+        return result.json()
+    }
+    return {
+        searchData,
+        getDetails
+    }
+})
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,51 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonContent,
+    IonHeader,
+    IonItem, IonLabel,
+    IonPage,
+    IonSearchbar,
+    IonSelect,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import './Home.css';
+import {useApi} from "../hooks/useApi";
+import {useEffect, useState} from "react";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+const Home = () => {
+    const {searchData} = useApi()
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [type, setType] = useState('')
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+        console.log('Search:', searchTerm)
+    }, [searchTerm]);
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Blank</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonSearchbar value={searchTerm}
+                              debounce={300}
+                              onIonChange={(e) => setSearchTerm(e.detail.value!)}>
+
+                </IonSearchbar>
+
+                <IonItem>
+                    <IonLabel>Select Search Type</IonLabel>
+                </IonItem>
+                <IonSelect value={type}
+                           onIonChange={(e) => setType(e.detail.value)}>
+                </IonSelect>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
